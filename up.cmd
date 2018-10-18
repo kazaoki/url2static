3a307c9ef00ec2d7c3a6de069c9bb30afefe5ecf
Tatsuya Karino <karino@kazaoki.jp>
2018-10-18 17:26:59 +0900

    [UPDATE] dockerが落ちても再起動するように修正。

--- up.cmd
+++ up.cmd
@@ -1,1 +1,1 @@
-docker run -v %CD%/app:/app -p 3412:8080 --name url2static kazaoki/puppeteer &
+docker run -v %CD%/app:/app -p 3412:8080 --name url2static --restart=always kazaoki/puppeteer &
--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v $(pwd)/app:/app -p 3412:8080 --name url2static kazaoki/puppeteer &
+docker run -v $(pwd)/app:/app -p 3412:8080 --name url2static --restart=always kazaoki/puppeteer &
